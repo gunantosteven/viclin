4bf96326c50ee1dc3f5f2724f5a01dcda6dd3b2a
Steven Gunanto <gunantosteven@gmail.com>
2015-06-23 09:32:49 +0700

    discard unused function

--- local/resources/views/owner/app.blade.php
+++ local/resources/views/owner/app.blade.php
@@ -81,14 +81,11 @@
         
         <div class="admin">
             <div class="image">
-                <img src="{{ asset('img/users/aqvatarius.jpg') }}" class="img-polaroid"/>                
+                <img src="{{ asset('img/users/owner.png') }}" class="img-polaroid"/>                
             </div>
             <ul class="control">                
                 <li><span class="icon-share-alt"></span> <a href="{{ url('/auth/logout') }}">Logout</a></li>
             </ul>
-            <div class="info">
-                <span>Welcom back! Your last visit: 24.10.2012 in 19:55</span>
-            </div>
         </div>
         
         <ul class="navigation">            
@@ -221,37 +218,6 @@
             <div id="menuDatepicker"></div>
         </div>
         
-        <div class="dr"><span></span></div>
-        
-        <div class="widget">
-
-            <div class="input-append">
-                <input id="appendedInputButton" style="width: 118px;" type="text"><button class="btn" type="button">Search</button>
-            </div>            
-            
-        </div>
-        
-        <div class="dr"><span></span></div>
-        
-        <div class="widget-fluid">
-            
-            <div class="wBlock clearfix">
-                <div class="dSpace">
-                    <h3>Last visits</h3>
-                    <span class="number">6,302</span>                    
-                    <span>5,774 <b>unique</b></span>
-                    <span>3,512 <b>returning</b></span>
-                </div>
-                <div class="rSpace">
-                    <h3>Today</h3>
-                    <span class="mChartBar" sparkType="bar" sparkBarColor="white"><!--240,234,150,290,310,240,210,400,320,198,250,222,111,240,221,340,250,190--></span>                                                                                
-                    <span>&nbsp;</span>
-                    <span>65% <b>New</b></span>
-                    <span>35% <b>Returning</b></span>
-                </div>
-            </div>
-            
-        </div>
         
     </div>
         
