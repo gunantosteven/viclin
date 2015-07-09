5c4b870f82cb56b0ef113dca5b573a5c7ad1b736
Steven Gunanto <gunantosteven@gmail.com>
2015-07-09 13:23:49 +0700

    change dashboard page

--- local/resources/views/admin/dashboard.blade.php
+++ local/resources/views/admin/dashboard.blade.php
@@ -3,6 +3,18 @@
 @section('content')
 
                                     
-Ini Admin
+<div class="row-fluid">
+                
+    <div class="span12">
+        <div class="head clearfix">
+            <div class="isb-user"></div>
+            <h1>Welcome Admin</h1>
+        </div>
+        <div class="block">
+            <center><h1>Welcome to page Admin</h1></center>
+        </div>
+    </div>
+                
+</div>
 
 @endsection
--- local/resources/views/owner/dashboard.blade.php
+++ local/resources/views/owner/dashboard.blade.php
@@ -1,5 +1,19 @@
 @extends('/owner/app')
 
 @section('content')
-Ini Owner
+
+<div class="row-fluid">
+                
+    <div class="span12">
+        <div class="head clearfix">
+            <div class="isb-user"></div>
+            <h1>Welcome Owner</h1>
+        </div>
+        <div class="block">
+            <center><h1>Welcome to page Owner</h1></center>
+        </div>
+    </div>
+                
+</div>
+
 @endsection
